9926bb34c18fd0a88d7d2e43b6a57fd94217429a
samwel-chege <samwelchegeh09@gmail.com>
2022-04-02 20:09:19 +0300

    store js

---
+++ features/distance/distanceSlice.js
@@ -0,0 +1,11 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+const intialState  = 0;
+export const distanceSlice = createSlice({
+    name:"distance",
+    intialState,
+    reducers:{
+        
+    }
+})
